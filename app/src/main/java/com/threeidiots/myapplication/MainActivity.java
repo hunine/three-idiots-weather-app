49f989dac198abce65b888bfd659ec00726a5c24
Kreazy <102190042@sv.dut.udn.vn>
2022-05-28 08:11:47 +0700

    done viewpager

--- app/src/main/java/com/threeidiots/myapplication/MainActivity.java
+++ app/src/main/java/com/threeidiots/myapplication/MainActivity.java
@@ -1,14 +1,75 @@
 package com.threeidiots.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_PAGE = 3;
+    private ViewPager2 viewpage;
+    private FragmentStateAdapter pageradapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+//        getActionBar().hide();
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        viewpage = findViewById(R.id.pager);
+
+
+
+
+
+        pageradapter = new ScreenSlideAdapter(this);
+        viewpage.setAdapter(pageradapter);
     }
+
+    private class ScreenSlideAdapter extends FragmentStateAdapter {
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 0:
+                    return new ViewpagerScreen("First Screen");
+
+                case 1:
+                    return new ViewpagerScreen("Second Screen");
+                case 2:
+                    return new ViewpagerScreen("Third Screen");
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGE;
+        }
+
+        public ScreenSlideAdapter(MainActivity mainActivity) {
+            super(mainActivity);
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/threeidiots/myapplication/ViewpagerScreen.java
+++ app/src/main/java/com/threeidiots/myapplication/ViewpagerScreen.java
@@ -1,18 +1,38 @@
-package com.mobile.threeidiots;
+package com.threeidiots.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Fragment1 extends Fragment {
+public class ViewpagerScreen extends Fragment {
+
+    public ViewpagerScreen(String str){
+        txtScreenNum = str;
+    }
+
+
+    private TextView txt;
+    private String txtScreenNum;
+    private View inflatedview = null;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return (ViewGroup)inflater.inflate(R.layout.fragment1, container, false);
+
+
+        inflatedview = inflater.inflate(R.layout.fragment, container, false);
+        txt = inflatedview.findViewById(R.id.txtScreenNum);
+        txt.setText(txtScreenNum);
+        txt.setTextColor(Color.GREEN);
+
+        return inflatedview;
+
     }
 }
