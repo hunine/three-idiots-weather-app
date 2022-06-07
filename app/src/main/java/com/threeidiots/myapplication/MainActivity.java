49429a8f150360cf6daae5d673be7ef76f4a97a6
Kreazy <102190042@sv.dut.udn.vn>
2022-06-07 08:56:19 +0700

    fix some UI

--- app/src/main/java/com/threeidiots/myapplication/MainActivity.java
+++ app/src/main/java/com/threeidiots/myapplication/MainActivity.java
@@ -207,38 +207,38 @@ public class MainActivity extends AppCompatActivity {
                 }).check();
 
 //      API
-        apiService = new WeatherApiService();
-
-        apiService.getWeatherList()
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<WeatherList>() {
-                    @Override
-                    public void onSuccess(@NonNull WeatherList weatherList) {
-                        Log.d("DEBUG1", "Success");
-                        weathernow = weatherList.getWeathers().get(0);
-
-                        for (Weather weather: weatherList.getWeathers()) {
-                            Log.d("SPACE", "----------------------------------------");
-                            Log.d("Datetime Forecasted", weather.getDateTimeForecasted());
-                            Log.d("DT", Integer.toString(weather.getDt()));
-                            Log.d("Temperature", Double.toString(weather.getWeatherMain().getTemperature()));
-                            Log.d("Feels like", Double.toString(weather.getWeatherMain().getFeelsLike()));
-                            Log.d("Humidity", Integer.toString(weather.getWeatherMain().getHumidity()));
-                            Log.d("Weather name", weather.getWeatherInfoList().get(0).getName());
-                            Log.d("Description", weather.getWeatherInfoList().get(0).getDescription());
-                            Log.d("Wind speed", Double.toString(weather.getWind().getSpeed()));
-
-                        }
-                        Log.d("City", weatherList.getLocation().getCity());
-                        Log.d("Country", weatherList.getLocation().getCountry());
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                            Log.d("DEBUG1", "Fail " + e.getMessage());
-                    }
-                });
+//        apiService = new WeatherApiService();
+//
+//        apiService.getWeatherList()
+//                .subscribeOn(Schedulers.newThread())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeWith(new DisposableSingleObserver<WeatherList>() {
+//                    @Override
+//                    public void onSuccess(@NonNull WeatherList weatherList) {
+//                        Log.d("DEBUG1", "Success");
+//                        weathernow = weatherList.getWeathers().get(0);
+//
+//                        for (Weather weather: weatherList.getWeathers()) {
+//                            Log.d("SPACE", "----------------------------------------");
+//                            Log.d("Datetime Forecasted", weather.getDateTimeForecasted());
+//                            Log.d("DT", Integer.toString(weather.getDt()));
+//                            Log.d("Temperature", Double.toString(weather.getWeatherMain().getTemperature()));
+//                            Log.d("Feels like", Double.toString(weather.getWeatherMain().getFeelsLike()));
+//                            Log.d("Humidity", Integer.toString(weather.getWeatherMain().getHumidity()));
+//                            Log.d("Weather name", weather.getWeatherInfoList().get(0).getName());
+//                            Log.d("Description", weather.getWeatherInfoList().get(0).getDescription());
+//                            Log.d("Wind speed", Double.toString(weather.getWind().getSpeed()));
+//
+//                        }
+//                        Log.d("City", weatherList.getLocation().getCity());
+//                        Log.d("Country", weatherList.getLocation().getCountry());
+//                    }
+//
+//                    @Override
+//                    public void onError(@NonNull Throwable e) {
+//                            Log.d("DEBUG1", "Fail " + e.getMessage());
+//                    }
+//                });
     }
 
     private class ScreenSlideAdapter extends FragmentStateAdapter {
