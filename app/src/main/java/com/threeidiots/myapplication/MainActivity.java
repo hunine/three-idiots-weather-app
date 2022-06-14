7c3cb103489d0b8578f99ee294050b718ad375f1
Kreazy <102190042@sv.dut.udn.vn>
2022-06-14 20:04:10 +0700

    this is final

--- app/src/main/java/com/threeidiots/myapplication/MainActivity.java
+++ app/src/main/java/com/threeidiots/myapplication/MainActivity.java
@@ -2,75 +2,47 @@ package com.threeidiots.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.net.Uri;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
-import android.os.Looper;
-import android.provider.Settings;
-import android.util.Log;
-
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
-import com.karumi.dexter.BuildConfig;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
 import com.threeidiots.myapplication.model.Weather;
 import com.threeidiots.myapplication.model.WeatherList;
-import com.threeidiots.myapplication.viewmodel.WeatherApiService;
-import java.util.List;
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.observers.DisposableSingleObserver;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.tabs.TabLayout;
-import java.util.List;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private static final int NUM_PAGE = 3;
     private ViewPager2 viewpage;
+    private int viewpager_position =0;
     private FragmentStateAdapter pageradapter;
 
     private TextView txtWindy;
     private TextView txtHum;
     private TextView txtFeellike;
     private TextView txtPrecipation;
+    private TextView txtCity;
     private TextView txtTime;
+
+    private ImageButton btnLocation;
     private Button btnHourly;
     private Button btnDaily;
     private Button btnWeekly;
+
+    private int pre_pressure =0;
+    private int pre_feellike =0;
+    private int pre_windy =0;
+    private int pre_humidity =0;
 
 
     @Override
@@ -78,10 +50,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Gson gson = new Gson();
-        Weather weathernow = gson.fromJson(getIntent().getStringExtra("now"), Weather.class);
-        Weather weatherdaily = gson.fromJson(getIntent().getStringExtra("daily"), Weather.class);
-        Weather weatherweekly = gson.fromJson(getIntent().getStringExtra("weekly"), Weather.class);
-        System.out.println(String.valueOf(weatherdaily.getWeatherMain().getFeelsLike()));
+        WeatherList weather1 = gson.fromJson(getIntent().getStringExtra("city1"), WeatherList.class);
+//        Weather weathernow1 = weather1.getWeathers().get(0);
+        Weather weatherdaily1 = weather1.getWeathers().get(7);
+//        Weather weatherweekly1 = weather1.getWeathers().get(32);
+//        System.out.println(weather1.getLocation().getCity().toString());
+
+
+        WeatherList weather2 = gson.fromJson(getIntent().getStringExtra("city2"), WeatherList.class);
+//        Weather weathernow2 = weather2.getWeathers().get(0);
+//        Weather weatherdaily2 = weather2.getWeathers().get(7);
+//        Weather weatherweekly2 = weather2.getWeathers().get(32);
+
+
+        WeatherList weather3 = gson.fromJson(getIntent().getStringExtra("city3"), WeatherList.class);
+//        Weather weathernow3 = weather3.getWeathers().get(0);
+//        Weather weatherdaily3 = weather3.getWeathers().get(7);
+//        Weather weatherweekly3 = weather3.getWeathers().get(32);
+
+//        Weather weatherweekly = gson.fromJson(getIntent().getStringExtra("weekly"), Weather.class);
+        System.out.println(String.valueOf(weatherdaily1.getWeatherMain().getFeelsLike()));
+
 
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
@@ -95,14 +84,18 @@ public class MainActivity extends AppCompatActivity {
         txtHum = findViewById(R.id.txtHumidity);
         txtPrecipation = findViewById(R.id.txtPrecitation);
         txtTime = findViewById(R.id.txt_time);
+        txtCity = findViewById(R.id.txtCity);
 
         btnHourly = findViewById(R.id.btn_hourly);
         btnDaily = findViewById(R.id.btn_daily);
         btnWeekly = findViewById(R.id.btn_weekly);
+        btnLocation = findViewById(R.id.button_location);
 
         pageradapter = new ScreenSlideAdapter(this);
         viewpage.setAdapter(pageradapter);
         viewpage.setOffscreenPageLimit(3);
+
+        SetTexthourly(weather1);
 
 
 
@@ -116,7 +109,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
-                SetText(weatherdaily);
+                txtTime.setText("Weather now");
+
+                switch (position)
+                {
+                    case 0:
+                        SetTexthourlySelected(weather1);
+                        viewpager_position = 0;
+                        txtCity.setText(weather1.getLocation().getCity());
+                        break;
+                    case 1:
+                        SetTexthourlySelected(weather2);
+                        viewpager_position = 1;
+                        txtCity.setText(weather2.getLocation().getCity());
+                        break;
+                    case 2:
+                        SetTexthourlySelected(weather3);
+                        viewpager_position = 2;
+                        txtCity.setText(weather3.getLocation().getCity());
+                        break;
+
+                }
             }
 
             @Override
@@ -129,8 +142,20 @@ public class MainActivity extends AppCompatActivity {
         btnHourly.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SetText(weathernow);
+                switch (viewpager_position){
+                    case 0:
+                        SetTexthourly(weather1);
+                        break;
+                    case 1:
+                        SetTexthourly(weather2);
+                        break;
+                    case 2:
+                        SetTexthourly(weather3);
+                        break;
+                }
                 txtTime.setText("Weather now");
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce2);
+                txtTime.startAnimation(anim);
                 ResetButtonColor();
                 btnHourly.setTextColor(getResources().getColor(R.color.textcolor));
             }
@@ -139,8 +164,21 @@ public class MainActivity extends AppCompatActivity {
         btnDaily.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SetText(weatherdaily);
-                txtTime.setText("Weather tommorow");
+                System.out.println(viewpager_position);
+                switch (viewpager_position){
+                    case 0:
+                        SetTextdaily(weather1);
+                        break;
+                    case 1:
+                        SetTextdaily(weather2);
+                        break;
+                    case 2:
+                        SetTextdaily(weather3);
+                        break;
+                }
+                txtTime.setText("Weather tomorow");
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce2);
+                txtTime.startAnimation(anim);
                 ResetButtonColor();
                 btnDaily.setTextColor(getResources().getColor(R.color.textcolor));
             }
@@ -149,19 +187,121 @@ public class MainActivity extends AppCompatActivity {
         btnWeekly.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SetText(weatherweekly);
+                switch (viewpager_position){
+                    case 0:
+                        SetTextweekly(weather1);
+                        break;
+                    case 1:
+                        SetTextweekly(weather2);
+                        break;
+                    case 2:
+                        SetTextweekly(weather3);
+                        break;
+                }
                 txtTime.setText("Weather this week");
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce2);
+                txtTime.startAnimation(anim);
                 ResetButtonColor();
                 btnWeekly.setTextColor(getResources().getColor(R.color.textcolor));
             }
         });
 
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewpage.setCurrentItem(0, true);
+            }
+        });
+
     }
 
-    private void SetText(Weather weather){
+    private void SetTexthourly(WeatherList weatherList){
+        Weather weather = weatherList.getWeathers().get(0);
         txtFeellike.setText(String.valueOf(Math.round(weather.getWeatherMain().getFeelsLike())) + " °C");
         txtHum.setText(String.valueOf(weather.getWeatherMain().getHumidity()) + " %");
-        txtWindy.setText(String.valueOf(Math.round(weather.getWind().getSpeed()) * 3.6) + " km/h");
+        txtWindy.setText(String.valueOf(Math.round(weather.getWind().getSpeed() * 3.6)) + " km/h");
+        txtPrecipation.setText(String.valueOf(weather.getWeatherMain().getPressure()) + " hPa");
+    }
+
+    private void SetTexthourlySelected(WeatherList weatherList){
+
+        pre_feellike = Integer.parseInt(txtFeellike.getText().toString().split("°")[0].trim());
+        pre_humidity = Integer.parseInt(txtHum.getText().toString().split("%")[0].trim());
+        pre_windy = Integer.parseInt(txtWindy.getText().toString().split("km/h")[0].trim());
+        pre_pressure = Integer.parseInt(txtPrecipation.getText().toString().split("hPa")[0].trim());
+
+        Weather weather = weatherList.getWeathers().get(0);
+
+        int feellike = (int) weather.getWeatherMain().getFeelsLike();
+        startCountAnimationFell(txtFeellike, pre_feellike, feellike);
+
+        int hummidity = (int) weather.getWeatherMain().getHumidity();
+        startCountAnimationHum(txtHum, pre_humidity, hummidity);
+
+        int windy = (int) weather.getWind().getSpeed();
+        startCountAnimationWin(txtWindy, pre_windy, windy);
+
+        int pressure = (int) weather.getWeatherMain().getPressure();
+        startCountAnimationPre(txtPrecipation, pre_pressure, pressure);
+//        txtFeellike.setText(String.valueOf(Math.round(weather.getWeatherMain().getFeelsLike())) + " °C");
+//        txtHum.setText(String.valueOf(weather.getWeatherMain().getHumidity()) + " %");
+//        txtWindy.setText(String.valueOf(Math.round(weather.getWind().getSpeed() * 3.6)) + " km/h");
+//        txtPrecipation.setText(String.valueOf(weather.getWeatherMain().getPressure()) + " hPa");
+    }
+
+    private void startCountAnimationFell(TextView txtview, int first_temp, int second_temp) {
+        ValueAnimator animator = ValueAnimator.ofInt(first_temp, second_temp);
+        animator.setDuration(1000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                txtview.setText(animation.getAnimatedValue().toString() + "°");
+            }
+        });
+        animator.start();
+    }
+    private void startCountAnimationHum(TextView txtview, int first_temp, int second_temp) {
+        ValueAnimator animator = ValueAnimator.ofInt(first_temp, second_temp);
+        animator.setDuration(1000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                txtview.setText(animation.getAnimatedValue().toString() + " %");
+            }
+        });
+        animator.start();
+    }
+    private void startCountAnimationWin(TextView txtview, int first_temp, int second_temp) {
+        ValueAnimator animator = ValueAnimator.ofInt(first_temp, second_temp);
+        animator.setDuration(1000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                txtview.setText(animation.getAnimatedValue().toString() + " km/h");
+            }
+        });
+        animator.start();
+    }
+    private void startCountAnimationPre(TextView txtview, int first_temp, int second_temp) {
+        ValueAnimator animator = ValueAnimator.ofInt(first_temp, second_temp);
+        animator.setDuration(1000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                txtview.setText(animation.getAnimatedValue().toString() + " hPa");
+            }
+        });
+        animator.start();
+    }
+
+    private void SetTextdaily(WeatherList weatherList){
+        Weather weather = weatherList.getWeathers().get(7);
+        txtFeellike.setText(String.valueOf(Math.round(weather.getWeatherMain().getFeelsLike())) + " °C");
+        txtHum.setText(String.valueOf(weather.getWeatherMain().getHumidity()) + " %");
+        txtWindy.setText(String.valueOf(Math.round(weather.getWind().getSpeed() * 3.6)) + " km/h");
+        txtPrecipation.setText(String.valueOf(weather.getWeatherMain().getPressure()) + " hPa");
+    }
+    private void SetTextweekly(WeatherList weatherList){
+        Weather weather = weatherList.getWeathers().get(32);
+        txtFeellike.setText(String.valueOf(Math.round(weather.getWeatherMain().getFeelsLike())) + " °C");
+        txtHum.setText(String.valueOf(weather.getWeatherMain().getHumidity()) + " %");
+        txtWindy.setText(String.valueOf(Math.round(weather.getWind().getSpeed() * 3.6)) + " km/h");
         txtPrecipation.setText(String.valueOf(weather.getWeatherMain().getPressure()) + " hPa");
     }
 
@@ -177,15 +317,24 @@ public class MainActivity extends AppCompatActivity {
         public Fragment createFragment(int position) {
 
             Gson gson2 = new Gson();
-            Weather weathernow2 = gson2.fromJson(getIntent().getStringExtra("now"), Weather.class);
+            WeatherList weather1 = gson2.fromJson(getIntent().getStringExtra("city1"), WeatherList.class);
+            Weather weathernow1 = weather1.getWeathers().get(0);
+
+
+            WeatherList weather2 = gson2.fromJson(getIntent().getStringExtra("city2"), WeatherList.class);
+            Weather weathernow2 = weather2.getWeathers().get(0);
+
+
+            WeatherList weather3 = gson2.fromJson(getIntent().getStringExtra("city3"), WeatherList.class);
+            Weather weathernow3 = weather3.getWeathers().get(0);
 
             switch (position){
                 case 0:
-                    return new ViewpagerScreen(weathernow2.getWeatherMain().getTemperature(), getPackageName(), R.raw.video1, weathernow2.getWeatherInfoList().get(0).getIcon(), weathernow2.getWeatherInfoList().get(0).getDescription(), weathernow2.getWeatherInfoList().get(0).getName());
+                    return new ViewpagerScreen(weathernow1.getWeatherMain().getTemperature(), getPackageName(), R.raw.video1, weathernow1.getWeatherInfoList().get(0).getIcon(), weathernow1.getWeatherInfoList().get(0).getDescription(), weathernow1.getWeatherInfoList().get(0).getName());
                 case 1:
                     return new ViewpagerScreen(weathernow2.getWeatherMain().getTemperature(), getPackageName(), R.raw.video2, weathernow2.getWeatherInfoList().get(0).getIcon(),weathernow2.getWeatherInfoList().get(0).getDescription(), weathernow2.getWeatherInfoList().get(0).getName());
                 case 2:
-                    return new ViewpagerScreen(weathernow2.getWeatherMain().getTemperature(), getPackageName(), R.raw.video3, weathernow2.getWeatherInfoList().get(0).getIcon(), weathernow2.getWeatherInfoList().get(0).getDescription(), weathernow2.getWeatherInfoList().get(0).getName());
+                    return new ViewpagerScreen(weathernow3.getWeatherMain().getTemperature(), getPackageName(), R.raw.video3, weathernow3.getWeatherInfoList().get(0).getIcon(), weathernow3.getWeatherInfoList().get(0).getDescription(), weathernow3.getWeatherInfoList().get(0).getName());
                 default:
                     return null;
             }
